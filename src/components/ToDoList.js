bbf8b928bea03cc19153cfee7ff68eb4bdfe959c
Johnny <johnnyyvard@me.com>
2020-12-07 11:30:51 +0100

    Initialisation du projet

---
+++ src/components/ToDoList.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ToDo from './ToDo'
+
+const ToDoList = ({ tasks, match, onToggleCompleted }) => {
+    let filteredTasks
+
+    switch (match.params.filter) {
+        case 'completed':
+            filteredTasks = tasks.filter(task => task.completed)
+            break;
+
+        default:
+            filteredTasks = tasks
+            break;
+    }
+
+    if (filteredTasks.length === 0) {
+        return (
+            <>
+                <h1 className="m-3">Liste de tâches</h1>
+                <ul className="list-group m-3">
+                    <li className="list-group-item">Aucune tâche à afficher.</li>
+                </ul>
+            </>
+
+        )
+
+    } else {
+        return (
+            <>
+                <h1 className="m-3">Liste de tâches</h1>
+                <ul className="list-group m-3">
+                    {
+                        filteredTasks.map((task) => <ToDo task={task} key={task.id} onToggleCompleted={onToggleCompleted} />)
+                    }
+                </ul>
+            </>
+
+        )
+
+    }
+}
+
+export default ToDoList
