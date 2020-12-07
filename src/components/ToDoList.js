c076278738dbed1e918003f43e64df74bdf33151
Johnny <johnnyyvard@me.com>
2020-12-07 12:12:41 +0100

    Mise à jour du composant ToDoList

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,44 +1,23 @@
-import React from 'react'
-import ToDo from './ToDo'
+import React, { Component } from 'react';
+import { observer, inject } from 'mobx-react';
+import ToDo from './ToDo';
 
-const ToDoList = ({ tasks, match, onToggleCompleted }) => {
-    let filteredTasks
-
-    switch (match.params.filter) {
-        case 'completed':
-            filteredTasks = tasks.filter(task => task.completed)
-            break;
-
-        default:
-            filteredTasks = tasks
-            break;
-    }
-
-    if (filteredTasks.length === 0) {
-        return (
-            <>
-                <h1 className="m-3">Liste de tâches</h1>
-                <ul className="list-group m-3">
-                    <li className="list-group-item">Aucune tâche à afficher.</li>
-                </ul>
-            </>
-
-        )
-
-    } else {
+@inject(store)
+@observer
+class ToDoList extends Component {
+    render() {
+        const store = this.props.store;
         return (
             <>
                 <h1 className="m-3">Liste de tâches</h1>
                 <ul className="list-group m-3">
                     {
-                        filteredTasks.map((task) => <ToDo task={task} key={task.id} onToggleCompleted={onToggleCompleted} />)
+                        store.tasks.map((task, idx) => <ToDo task={task} key={idx} />)
                     }
                 </ul>
             </>
-
         )
-
     }
 }
 
-export default ToDoList
+export default ToDoList;
